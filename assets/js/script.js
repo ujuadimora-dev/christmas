289e55b0de8c340c8884738f84c943ba79566dd7
Adimora Uju Louisa <adimorauju@gmail.com>
2023-12-10 01:20:39 +0000

    Add the Audio file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,44 @@
+// Get the audio element
+const backgroundMusic = document.getElementById('backgroundMusic');
+
+// Get the countdown elements
 const days = document.getElementById('days');
 const hours = document.getElementById('hours');
 const minutes = document.getElementById('minutes');
 const seconds = document.getElementById('seconds');
 
-let countdownDate = "2023-12-25T00:00";
-let countDown = new Date(countdownDate).getTime();
+// Set the countdown date
+const countdownDate = new Date('2023-12-25T00:00').getTime();
 
-let updateTime = setInterval(function(){
-    let now = new Date().getTime();  // Corrected this line
+// Update the countdown timer
+function updateCountdown() {
+    const now = new Date().getTime();
+    const difference = countdownDate - now;
 
-    let difference = countDown - now;
+    const daysValue = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hoursValue = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesValue = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsValue = Math.floor((difference % (1000 * 60)) / 1000);
 
-    let dys = Math.floor(difference / (1000 * 60 * 60 * 24));
-    let hrs = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let mins = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    let secs = Math.floor((difference % (1000 * 60)) / 1000);
-    
-    days.innerHTML = dys < 10 ? "0" + dys : dys;
-    hours.innerHTML = hrs < 10 ? "0" + hrs : hrs;
-    minutes.innerHTML = mins < 10 ? "0" + mins : mins;
-    seconds.innerHTML = secs < 10 ? "0" + secs : secs;
+    days.innerHTML = daysValue < 10 ? '0' + daysValue : daysValue;
+    hours.innerHTML = hoursValue < 10 ? '0' + hoursValue : hoursValue;
+    minutes.innerHTML = minutesValue < 10 ? '0' + minutesValue : minutesValue;
+    seconds.innerHTML = secondsValue < 10 ? '0' + secondsValue : secondsValue;
+}
 
-}, 1000);
+// Update the countdown every second
+const updateTime = setInterval(updateCountdown, 1000);
+
+// Play background music when the page is clicked
+document.addEventListener('click', function () {
+    backgroundMusic.play();
+});
+
+// Pause background music when the page is not in focus
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        backgroundMusic.pause();
+    } else {
+        backgroundMusic.play();
+    }
+});
